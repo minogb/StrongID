8ffd8f51efdcf66e1079ef142e5d7caddae2b4d8
minogb <bjminogue@gmail.com>
2026-05-12 19:45:09 -0400

    Add project files.

---
+++ StrongID/ID.cs
@@ -0,0 +1,92 @@
+﻿using System.Numerics;
+
+namespace StrongID {
+    public interface IID<T> where T : IComparable {
+        public abstract T Value { get; }
+    }
+    public class GenericID<T, J>: IID<J>, IComparable<GenericID<T, J>> where J : IComparable {
+        protected J ProtectedValue;
+
+        public J Value =>  ProtectedValue;
+
+        public GenericID(J value) {
+            if(Value == null) throw new ArgumentNullException("ID must have a non-null value");
+            ProtectedValue = value;
+        }
+        public static explicit operator J(GenericID<T,J> id) {
+            return id.Value;
+        }
+        public static explicit operator GenericID<T, J>(J id) {
+            return new GenericID<T,J>(id);
+        }
+        public override string ToString() {
+#pragma warning disable CS8603 // Possible null reference return. Its marked as not null.
+            return Value.ToString();
+#pragma warning restore CS8603 // Possible null reference return.
+        }
+        public override bool Equals(object? obj) {
+            var other = obj as GenericID<T, J>;
+#pragma warning disable CS8604 // Possible null reference argument.
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+            if (other == null) return false;
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+#pragma warning restore CS8604 // Possible null reference argument.
+            return other.Value.Equals(Value);
+        }
+        public override int GetHashCode() {
+            return Value.GetHashCode();
+        }
+        public int CompareTo(object? obj) {
+            var other = obj as GenericID<T,J>;
+            return this.CompareTo(other);
+        }
+
+        public int CompareTo(GenericID<T, J>? other) {
+#pragma warning disable CS8604 // Possible null reference argument.
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+            if (other == null) return -1;
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+#pragma warning restore CS8604 // Possible null reference argument.
+            return this.Value.CompareTo(other.Value);
+        }
+        public static bool operator ==(GenericID<T, J> left, GenericID<T, J> right) {
+            if (ReferenceEquals(left, null)) {
+                return ReferenceEquals(right, null);
+            }
+
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(GenericID<T, J> left, GenericID<T, J> right) {
+            return !(left == right);
+        }
+
+        public static bool operator <(GenericID<T, J> left, GenericID<T, J> right) {
+            return ReferenceEquals(left, null) ? !ReferenceEquals(right, null) : left.CompareTo(right) < 0;
+        }
+
+        public static bool operator <=(GenericID<T, J> left, GenericID<T, J> right) {
+            return ReferenceEquals(left, null) || left.CompareTo(right) <= 0;
+        }
+
+        public static bool operator >(GenericID<T, J> left, GenericID<T, J> right) {
+            return !ReferenceEquals(left, null) && left.CompareTo(right) > 0;
+        }
+
+        public static bool operator >=(GenericID<T, J> left, GenericID<T, J> right) {
+            return ReferenceEquals(left, null) ? ReferenceEquals(right, null) : left.CompareTo(right) >= 0;
+        }
+    }
+    public class ID<T> : GenericID<T, int> {
+        public ID(int value) : base(value) {
+        }
+    }
+    public class BigID<T> : GenericID<T, BigInteger> {
+        public BigID(BigInteger value) : base(value) {
+        }
+    }
+    public class LooseID<T> : GenericID<T, string> {
+        public LooseID(string value) : base(value) {
+        }
+    }
+}
---
+++ TestApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using StrongID;
+
+namespace TestApp {
+    internal class Program {
+        static void Main(string[] args) {
+            GenericID<Program, int> value = new GenericID<Program, int>(1);
+            GenericID<Program, int> value2 = new GenericID<Program, int>(1);
+            GenericID<FileInfo, int> value3 = new GenericID<FileInfo, int>(1);
+
+            ID<Program> valu4= new ID<Program>(1);
+
+            List<GenericID<Program, int>> List = new();
+            List.Add(value);
+            List.Add(value2);
+            //List.Add(value3);
+
+
+            List<IID<int>> IDList = new();
+            IDList.Add(value);
+            IDList.Add(value2);
+            IDList.Add(value3); 
+            Console.WriteLine(value.CompareTo(value2) == 0 ? "equal" : "not equal");
+            Console.WriteLine(value.Equals(value2) ? "equal" :"not equal");
+            Console.WriteLine(value == value2 ? "equal" : "not equal");
+            Console.WriteLine(value == valu4 ? "equal" : "not equal");
+            Console.WriteLine(value);
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine(value.Equals(1) ? "equal" : "not equal");
+            Console.WriteLine(value.Equals(value3) ? "equal" : "not equal");
+            List.Sort();
+
+        }
+    }
+}
